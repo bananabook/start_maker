c8d5750b0709aa4b0fc5d557143e21d7faa7b7b1
none <none>
2024-09-24 21:51:27 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# start page maker
+
+Create a startpage, where you just need to press a single key to open the page you want.
+
+## howto
+
+Place the pages you want to include in `config.json` run `go run main.go > made.html`.
+
+Once you have opened the page, press the key assinged to the page to open it in a new background tab. Want to open it in the current tab? Press shift+key. This means, you cannot assign upper case and lower case of the same letter to different pages.
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"html/template"
+	"os"
+	"encoding/json"
+	"log"
+	//"strings"
+	"unicode"
+	"strings"
+)
+
+type Entry struct{
+	Keys []string
+	Name string
+	URL template.URL
+}
+type Data struct{
+	Entries []Entry
+}
+func main() {
+	data,_:=readData("config.json")
+	//Augment(&data)
+	UseTemplate(data)
+}
+func getData()Data{
+	data:=struct{
+		Entries []Entry
+	}{
+		Entries: []Entry{
+			Entry{
+				Keys: []string{"d","D"},
+				Name: "Duolingo",
+				URL: template.URL("https://www.duolingo.com"),
+			},
+			Entry{
+				Keys: []string{"s","S"},
+				Name: "DuckDuckGo",
+				URL: template.URL("https://duckduckgo.com/"),
+			},
+			Entry{
+				Keys: []string{"w","W"},
+				Name: "WhatsApp",
+				URL: template.URL("https://web.whatsapp.com/"),
+			},
+		},
+	}
+	return data
+}
+func readData(filename string)(Data,error){
+	f,e:=os.Open(filename)
+	if e!=nil{
+		return Data{}, e
+	}
+	defer f.Close()
+	data:=Data{}
+	json.NewDecoder(f).Decode(&data)
+	return data,nil
+}
+func UseTemplate(data Data){
+	t := template.Must(template.New("template.html").Funcs(template.FuncMap{
+    "toup":strings.ToUpper,
+    "isup":func(input string)bool{
+      return input == strings.ToUpper(input)
+    },
+    "tolow":strings.ToLower,
+    "islow":func(input string)bool{
+      return input == strings.ToLower(input)
+    },
+  }).ParseFiles("template.html"))
+	t.Execute(os.Stdout,data)
+}
+func writeData(filename string, data Data) {
+	f,e:=os.Create(filename)
+	if e!=nil{
+		log.Println(e)
+	}
+	defer f.Close()
+	enc:=json.NewEncoder(f)
+	enc.SetIndent(""," ")
+	enc.Encode(data)
+}
+
+func Augment(input *Data){
+	for i:=range input.Entries{
+		input.Entries[i].Keys=AppendCase(input.Entries[i].Keys)
+	}
+}
+func AppendCase(input []string)([]string){
+	for _,v:=range input{
+		if unicode.IsUpper(rune(v[0])){
+			lower:=strings.ToLower(v)
+			input=AppendIfNew(input,lower)
+		}else{
+			upper:=strings.ToUpper(v)
+			input=AppendIfNew(input,upper)
+		}
+	}
+	return input
+}
+func AppendIfNew(input []string,addition string)[]string{
+			found:=false
+			for _,v2:=range input{
+				if v2==addition{
+					found=true
+				}
+			}
+			if !found{
+				input=append(input,addition)
+			}
+			return input
+}
